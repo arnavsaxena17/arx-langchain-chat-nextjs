43561f69c8cd8b31534e02619131a71eb7cddcc2
Arnav Saxena <arnavsaxena17@gmail.com>
2024-03-07 19:39:19 +0530

    chnages

--- pages/api/chat.tsx
+++ pages/api/chat.tsx
@@ -5,15 +5,6 @@ import { RunnableSequence } from "@langchain/core/runnables";
 
 export default async function (req, res) {
 
-  // const response = await fetch(process.env.LCC_ENDPOINT_URL, {
-  //   method: "POST",
-  //   headers: { "Content-Type": "application/json", "X-Api-Key": process.env.LCC_TOKEN },
-  //   body: JSON.stringify({
-  //     question: req.body.question,
-  //     history: req.body.history
-  //   }),
-  // });
-
   const model = new ChatOpenAI({});
   const promptTemplate = PromptTemplate.fromTemplate(
   "Tell me a joke about {topic}"
